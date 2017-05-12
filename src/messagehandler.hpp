e8795483c81005e3e2880569bea6b8459e888f28
Benoît CORTIER <benoit.cortier@fried-world.eu>
2017-05-12 11:41:35 +0200

    Add missing license header.

--- src/messagehandler.hpp
+++ src/messagehandler.hpp
@@ -1,3 +1,13 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                               //
+// Copyright 2017 TiWinDeTea.                                                                    //
+// This file is part of Force3 project which is released under the                               //
+// European Union Public License v1.1. If a copy of the EUPL was                                 //
+// not distributed with this software, you can obtain one at :                                   //
+// https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence-eupl-v11     //
+//                                                                                               //
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
 #ifndef MESSAGEHANDLER_HPP
 #define MESSAGEHANDLER_HPP
 
