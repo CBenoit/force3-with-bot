3b3a06d988be21ac7fd2732c9dcb138238c3c75b
Benoît CORTIER <benoit.cortier@fried-world.eu>
2017-05-14 20:02:08 +0200

    Factorize default heuristic.

--- src/heuristic.cpp
+++ src/heuristic.cpp
@@ -3,6 +3,21 @@
 #include "heuristic.hpp"
 #include "boardstate.hpp"
 
+bool is_there_a_connected_token(const GameState& game_state, square::type type, uint_fast8_t current_x, uint_fast8_t current_y) {
+	for (uint_fast8_t j{BOARD_DIMENSION * BOARD_DIMENSION}; j--;) {
+		square::type square_type = game_state.get_board_state().get(j);
+		if (square_type == type) {
+			uint_fast8_t other_x = j % BOARD_DIMENSION, other_y = j / BOARD_DIMENSION;
+			uint_fast8_t x_diff = static_cast<uint_fast8_t>(std::abs(static_cast<int_fast8_t>(current_x - other_x)));
+			uint_fast8_t y_diff = static_cast<uint_fast8_t>(std::abs(static_cast<int_fast8_t>(current_y - other_y)));
+			if ((x_diff == 1 || y_diff == 1) && x_diff + y_diff <= 2) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
 auto heuristic::default_heuristic(const GameState& game_state, square::type player) -> return_t {
 	square::type winner;
 	if (game_state.is_there_a_winner(&winner)) {
@@ -21,30 +36,14 @@ auto heuristic::default_heuristic(const GameState& game_state, square::type play
 	for (uint_fast8_t i{BOARD_DIMENSION * BOARD_DIMENSION}; i--;) {
 		square::type square_type = game_state.get_board_state().get(i);
 		if (square_type == player) {
-			uint_fast8_t current_x = i % BOARD_DIMENSION, current_y = i / BOARD_DIMENSION;
-			for (uint_fast8_t j{BOARD_DIMENSION * BOARD_DIMENSION}; j--;) {
-				square::type square_type = game_state.get_board_state().get(j);
-				if (square_type == player) {
-					uint_fast8_t other_x = j % BOARD_DIMENSION, other_y = j / BOARD_DIMENSION;
-					uint_fast8_t x_diff = static_cast<uint_fast8_t>(std::abs(static_cast<int_fast8_t>(current_x - other_x)));
-					uint_fast8_t y_diff = static_cast<uint_fast8_t>(std::abs(static_cast<int_fast8_t>(current_y - other_y)));
-					if ((x_diff == 1 || y_diff == 1) && x_diff + y_diff <= 2) {
-						++score;
-					}
-				}
+			++score;
+			if (is_there_a_connected_token(game_state, player, i % BOARD_DIMENSION, i / BOARD_DIMENSION)) {
+				++score;
 			}
 		} else if (square_type == opponent) {
-			uint_fast8_t current_x = i % BOARD_DIMENSION, current_y = i / BOARD_DIMENSION;
-			for (uint_fast8_t j{BOARD_DIMENSION * BOARD_DIMENSION}; j--;) {
-				square::type square_type = game_state.get_board_state().get(j);
-				if (square_type == opponent) {
-					uint_fast8_t other_x = j % BOARD_DIMENSION, other_y = j / BOARD_DIMENSION;
-					uint_fast8_t x_diff = static_cast<uint_fast8_t>(std::abs(static_cast<int_fast8_t>(current_x - other_x)));
-					uint_fast8_t y_diff = static_cast<uint_fast8_t>(std::abs(static_cast<int_fast8_t>(current_y - other_y)));
-					if ((x_diff == 1 || y_diff == 1) && x_diff + y_diff <= 2) {
-						--score;
-					}
-				}
+			--score;
+			if (is_there_a_connected_token(game_state, opponent, i % BOARD_DIMENSION, i / BOARD_DIMENSION)) {
+				++score;
 			}
 		}
 	}
